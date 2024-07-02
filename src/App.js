0ef8aa2cfd1bc706fe01e34553856c11716c43fa
Олег  Трушнин <truoleg2004@gmail.com>
2024-07-02 15:22:30 +0300

    melochi

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import NavigationComponent from './components/navigationComponent/NavigationComponent';
 
 function App() {
   return (
-    <div>
+    <>
+      <NavigationComponent/>
       <Router>
         <Routes>
           <Route path='/host'/>
@@ -12,7 +14,7 @@ function App() {
           <Route path='/monitor'/>
         </Routes>
       </Router>
-    </div>
+    </>
   );
 }
 
--- src/components/navigationComponent/navigationComponent.jsx
+++ src/components/navigationComponent/navigationComponent.jsx
@@ -1,12 +1,11 @@
-import classes from './navigationStyles.module.scsss'
+import React from "react";
+import classes from './navigationStyles.module.scss';
 
 function navigationComponent (){
     return(
-        <>
-            <div className={classes.container}>
-
-            </div>
-        </>
+        <div className={classes.container}>
+            <h1>test</h1>
+        </div>
     )
 }
 export default navigationComponent;
